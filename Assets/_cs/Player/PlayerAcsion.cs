c66a61fe6861b0f2e6d40a3c01c72596a0f7cb73
22ci0401 <22ci0401@jec.ac.jp>
2023-11-21 13:42:13 +0900

    プレイヤー調整

--- Assets/_cs/Player/PlayerAcsion.cs
+++ Assets/_cs/Player/PlayerAcsion.cs
@@ -2,6 +2,7 @@ using SojaExiles;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAcsion : MonoBehaviour
@@ -10,33 +11,38 @@ public class PlayerAcsion : MonoBehaviour
     Transform parentObj;
 
     [SerializeField]
+    BoxCollider BoxCollider;
+
+    [SerializeField]
     Rigidbody parentRigidBody;
 
+    //キャッチしたオブジェクトの取得
     private Rigidbody HaveObject;
+    private Collider ObjectCollider;
+
+
     public playerTest playerTest;
+
+    private void Start()
+    {
+        BoxCollider.enabled = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(Input.GetMouseButtonDown(0));
-
         if(Input.GetMouseButtonDown(0))
         {
             // オブジェクトを持っていないときはオブジェクトを持つ
             if(HaveObject == null)
             {
-
                 float distance = 10;
                 var hits = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward, distance);
                 Debug.Log(hits);
-                foreach(var hit in hits)
+                foreach (var hit in hits)
                 {
                     if (hit.collider.tag == "Object")
                     {
-                        HaveObject = hit.collider.gameObject.GetComponent<Rigidbody>();
-                        HaveObject.constraints = RigidbodyConstraints.FreezePositionY;
-                        HaveObject.transform.position = transform.position + new Vector3(0, 1);
-                        HaveObject.isKinematic = true;
-                        HaveObject.transform.SetParent(parentObj.transform, true);
+                        ObjectCatch(hit.collider);
                     }
                     if(hit.collider.tag == "Item")
                     {
@@ -49,10 +55,26 @@ public class PlayerAcsion : MonoBehaviour
             {
                 HaveObject.constraints = 0;
                 HaveObject.isKinematic = false;
+                ObjectCollider.enabled  = true;
+                BoxCollider.enabled = false;
                 HaveObject.transform.SetParent(null, true);
                 HaveObject.velocity = parentRigidBody.velocity;
                 HaveObject = null;
+                ObjectCollider = null;
             }
         }
+    }
+
+    private void ObjectCatch(Collider othe)
+    {
+        ObjectCollider = othe;
+        HaveObject = ObjectCollider.gameObject.GetComponent<Rigidbody>();
+        ObjectCollider.enabled = false;  
+        BoxCollider.enabled = true;
+        HaveObject.constraints = RigidbodyConstraints.FreezePositionY;
+        HaveObject.isKinematic = true;
+        HaveObject.transform.SetParent(parentObj.transform, true);
+        HaveObject.transform.localPosition = new Vector3(0, 0.1f, 0.3f);
+        ObjectCollider.transform.rotation = transform.rotation;
     }
 }
