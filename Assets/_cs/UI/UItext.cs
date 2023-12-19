a404932293b459f029db480c93ac1a1a6f2119c7
0322 <test@jec.ac.jp>
2023-12-19 16:41:25 +0900

    ヒント文

--- Assets/_cs/UI/UItext.cs
+++ Assets/_cs/UI/UItext.cs
@@ -15,16 +15,25 @@ public class UItext : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //switch (num)
-        //    {
-        //    case 0:
-        //        text.text = "";
-        //        break;
-        //    case 1:
-                    
+        switch (num)
+        {
 
-        //}
-            
+            case 1:
+            text.text = "ネジが緩んでいる...";
+            break;
+        case 4:
+            text.text = "鈍器があれば割れそう...？";
+            break;
+            case 6:
+                text.text = "釘が錆びている...釘抜があれば抜けそう...？";
+                break;
+            case 12:
+                text.text = "何かが血で隠れてる...怖いけど布があればふき取れそうかな...？";
+                break;
 
+
+        }
+
+
+        }
     }
-}
