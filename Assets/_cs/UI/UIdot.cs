8acac6551f962b2e9728059e2b47bb0427dd94fb
22ci0401 <22ci0401@jec.ac.jp>
2023-12-19 12:25:23 +0900

    ドア調整

--- Assets/_cs/UI/UIdot.cs
+++ Assets/_cs/UI/UIdot.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class UIdot : MonoBehaviour
 {
-    GameObject cv;
+    public GameObject cv;
     // Start is called before the first frame update
     void Start()
     {
-        cv = GameObject.Find("Canvas");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        cv = GameObject.Find("Canvas");
+
         if(cv.GetComponent<Canvas>().enabled==false)
         {
             this.gameObject.GetComponent<Canvas>().enabled=true;
