7a934f6e3e4ffa0031b4a33f4cc1c297a64d78b6
krokke-jp <2088827@gmail.com>
2024-01-24 12:29:52 +0900

    エンディングのBGMを追加、

--- Assets/_cs/BGM.cs
+++ Assets/_cs/BGM.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class BGM : MonoBehaviour
 {
-    public AudioSource ac;
+    public AudioSource as1;
+    public AudioClip ac1;
+    public AudioClip ac2;
+    int end;
+
     // Start is called before the first frame update
     void Start()
     {
-        ac = GetComponent<AudioSource>();
-        ac.Play();
+        as1 = GetComponent<AudioSource>();
+        end = PlayerPrefs.GetInt("flag");
+        if (end == 1)
+        {
+            as1.PlayOneShot(ac1);
+        }
+        else
+        {
+            as1.PlayOneShot(ac2);
+        }
     }
 
     // Update is called once per frame
